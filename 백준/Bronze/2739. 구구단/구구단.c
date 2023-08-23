7d5f9044191163624d45a1e9eaeeb32fdbc24ec2
syunindaee <139154270+syunindaee@users.noreply.github.com>
2023-08-23 19:43:30 +0900

    [Bronze V] Title: 구구단, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+
+	int num;
+
+	scanf("%d", &num);
+
+	for (int i = 0; i < 9; i++)
+	{
+		printf("%d * %d = %d\n", num, i + 1, num * (i + 1));
+	}
+    
+	return 0;
+}
